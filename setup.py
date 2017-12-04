6a90c847990c3c0d070cdef25200a09fe2b4233f
a.strunin <a.strunin13@gmail.com>
2017-12-04 13:45:01 +0300

    Update setup.py to put yeelight-cinema.py to Scripts folder. Increment version

--- setup.py
+++ setup.py
@@ -3,10 +3,14 @@ from os.path import join, dirname
 
 setup(
     name='yeelight-cinema',
-    version='0.12',
+    version='0.13',
     packages=find_packages(),
     long_description="This script can be used with yeelight color bulb. "
                      "Script detects color of scene and changes bulb color",
+
+
+    scripts=['yeelight-cinema\yeelight-cinema.py'],
+
     install_requires=[
         'pillow',
         'pyscreenshot',
