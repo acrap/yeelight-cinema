16a2acd83843c59a6ebfd418b423152478edb377
acrap <acrapmonster@gmail.com>
2017-12-04 21:50:38 +0300

    0.14

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='yeelight-cinema',
-    version='0.13',
+    version='0.14',
     packages=find_packages(),
     long_description="This script can be used with yeelight color bulb. "
                      "Script detects color of scene and changes bulb color",
